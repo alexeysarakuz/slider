422101c449020aa0935ac20fa091f83f28c501dd
Alex <homahomchikov@gmail.com>
2019-03-09 20:41:15 +0200

    Sliding + opacity

--- js/common.js
+++ js/common.js
@@ -6,7 +6,7 @@ window.onload = () => {
   let currentSlide = 0;
   let zIndex = 2000;
   var clickable = true;
-  const howFast = 1000; //ms min 900-1500 recommended
+  const howFast = 1200; //ms min 1100-1500 recommended
 
   for(let i = 0; i < slide.length; i++){
     slide[i].style.zIndex = zIndex - (i * 10);
@@ -15,15 +15,17 @@ window.onload = () => {
   rightArrow.onclick = () => {
     if(clickable){
       clickable = false;
-
-      if(currentSlide == slide.length - 1){
-        slide[currentSlide].style.left = '-100%'
+      let q = slide.length - 2
+      if(currentSlide == slide.length - 2){
+        slide[currentSlide].style.width = '0%'
         slide[currentSlide].style.zIndex = '6000'
+        removeActiveClass(slide, currentSlide)
         currentSlide = 0;
 
+        slide[slide.length - 1].style.width = '0%';
         for(let i = 0; i < slide.length - 1; i++){
           slide[i].style.transition = '0s';
-          slide[i].style.left = '0';
+          slide[i].style.width = '100%';
           slide[i].style.zIndex = zIndex - ((i+10) * 100)
         }
 
@@ -34,22 +36,25 @@ window.onload = () => {
 
           slide[slide.length - 1].style.zIndex = zIndex - ((slide.length - 1) * 10);
           setTimeout(() => {
-            slide[slide.length - 1].style.left = '0';
+            slide[slide.length - 1].style.width = '100%';
           }, 1500)
 
           for(let i = 0; i < slide.length; i++){
-            slide[i].style.transition = '1.5s'
+            slide[i].style.transition = 'width 1.5s'
           }
-        }, 1000)
+        }, 1500)
 
         setTimeout(() => {
+          addActiveClass(slide, currentSlide);
           clickable = true;
-        }, howFast)
+        }, 1500)
 
       }else{
-        slide[currentSlide].style.left = '-100%'
+        slide[currentSlide].style.width = '0%'
+        removeActiveClass(slide, currentSlide)
         currentSlide++;
         setTimeout(() => {
+          addActiveClass(slide, currentSlide);
           clickable = true;
         }, howFast)
       }
@@ -60,41 +65,56 @@ window.onload = () => {
 
     if(clickable){
       clickable = false;
-      console.log(currentSlide)
       if(currentSlide == 0){
         for(let i = 0; i < slide.length; i++){
           slide[i].style.transition = '0s';
           if(i != 0){
-            slide[i].style.left = '-100%';
+            slide[i].style.width = '0';
             slide[i].style.zIndex = zIndex - (i+10) * 10
           }else{
             slide[i].style.zIndex = 30
           }
         }
         setTimeout(function(){
-          slide[slide.length-1].style.transition = '1.5s';
-          slide[slide.length-1].style.left = '0%';
+          slide[slide.length-1].style.transition = 'width 1.5s';
+          slide[slide.length-1].style.width = '100%';
 
           for(let i = 0; i < slide.length; i++){
-            slide[i].style.transition = '1.5s';
+            slide[i].style.transition = 'width 1.5s';
           }
         }, 100)
         setTimeout(() => {
+          currentSlide--;
           clickable = true;
         }, howFast)
-        currentSlide = slide.length;
+        removeActiveClass(slide, currentSlide)
+        currentSlide = slide.length - 1;
+        addActiveClass(slide, currentSlide);
       }else{
-        slide[0].style.left = '-100%'
+        slide[0].style.width = '0%'
         setTimeout(function(){
           slide[0].style.zIndex = 2000
-        }, 500)
+        }, 1500)
+        slide[currentSlide-1].style.width = '100%'
         setTimeout(() => {
+          removeActiveClass(slide, currentSlide)
+          currentSlide--;
+          addActiveClass(slide, currentSlide);
           clickable = true;
-        }, howFast)
-        slide[currentSlide-1].style.left = '0%'
+        }, 1500)
       }
-      currentSlide--;
     }
   }
 
+}
+
+const removeActiveClass = (slide, currentSlide) => {
+  setTimeout(() => {
+    slide[currentSlide].classList.remove('active');
+  }, 1500)
+}
+
+
+const addActiveClass = (slide, currentSlide) => {
+  slide[currentSlide].classList.add('active');
 }
